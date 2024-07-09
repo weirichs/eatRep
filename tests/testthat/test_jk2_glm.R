4cad7996d5df4e53669810821df895bd14f399b4
Sebastian Weirich <sebastian.weirich@iqb.hu-berlin.de>
2024-07-09 23:41:35 +0200

    It just never ends

--- tests/testthat/test_jk2_glm.R
+++ tests/testthat/test_jk2_glm.R
@@ -9,9 +9,9 @@ test_that("repGlm", {
                  formula = score~sex, family ="gaussian")))
   txt2 <- capture_output(res1 <- report(mod1, printGlm = TRUE))
   expect_equal(res1[which(res1[,"parameter"] == "(Intercept)"),"est"], c(508.406, 502.607, 526.287))
-  expect_equal(dim(res1), c(18, 9))
+  expect_equal(dim(res1), c(15, 9))
   expect_equal(unique(res1$group), c("countryA", "countryB", "countryC"))
-  expect_equal(unique(res1$parameter), c("(Intercept)", "Ncases", "sexmale", "Nvalid" , "R2", "R2nagel"  ))
+  expect_equal(unique(res1$parameter), c("(Intercept)", "Ncases", "sexmale", "Nvalid" , "R2"  ))
 })
 
 
